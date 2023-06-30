7b489e71d5bc65e0c9ed2100c5c1f2d90859ddc1
leeeunmi25 <akdongsam25@naver.com>
2023-06-30 14:36:06 +0900

    0630

--- vscode/hello/src/a0628/Ex3_16.java
+++ vscode/hello/src/a0628/Ex3_16.java
@@ -13,7 +13,7 @@ public class Ex3_16 {
 
         System.out.printf("ch=%c%n", ch); //c% 한개의 문자 C
         System.out.printf("ch < 'a' || ch > 'z'=%b%n", ch < 'a' || ch > 'z');
-        //true 소문자 a는 아스키 코드갓ㅂ이 97 대문자 C는 67 ch < 'a' 참이므로 ture
+        //true 소문자 a는 아스키 코드값이 97 대문자 C는 67 ch < 'a' 참이므로 ture
         System.out.printf("!('a'<=ch && ch<='z')=%b%n",!('a' <= ch && ch <='z'));
         //true
         System.out.printf("'a' <= ch && ch <= 'z' =%b%n", 'a'<=ch && ch<='z');
