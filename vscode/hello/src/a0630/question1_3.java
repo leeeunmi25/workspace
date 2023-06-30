d2c34e1e1e74697ba5a071f0f7ef66973c9da9a5
leeeunmi25 <akdongsam25@naver.com>
2023-06-30 17:52:47 +0900

    0630

--- vscode/hello/src/a0630/question1_3.java
+++ vscode/hello/src/a0630/question1_3.java
@@ -4,7 +4,9 @@ public class question1_3 {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
         int sum =0;
-
+        for (int i =0; i <arr.length; i++){
+            sum += arr[i];
+        }
 
         System.out.println("sum=" + sum);
     }
