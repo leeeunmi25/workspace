530ba836028bf8818d633059abcd1b4c29012e46
leeeunmi25 <akdongsam25@naver.com>
2023-06-30 16:05:58 +0900

    0630

---
+++ vscode/hello/src/a0630/Ea145_4_3.java
@@ -0,0 +1,13 @@
+package a0630;
+
+public class Ea145_4_3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int totalSum = 0;
+        for(int i=1; i <=10; i++){
+            sum +=i;
+            totalSum = totalSum + sum;
+        }
+        System.out.println("totalSum="+totalSum);
+    }
+}
---
+++ vscode/hello/src/a0630/Ea146_4_5.java
@@ -0,0 +1,10 @@
+package a0630;
+
+public class Ea146_4_5 {
+    public static void main(String[] args) {
+        for (int j = 0; j <= 10; j++){
+            System.out.println("*");
+        System.out.println();
+        }
+    } //end of main
+} //end of class
